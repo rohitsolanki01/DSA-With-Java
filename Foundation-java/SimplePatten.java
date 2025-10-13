63565790f1e54bfb5372e9e783882a819da6dab6
rohitsolanki01 <rohitsolanki0473@gmail.com>
2025-10-13 12:28:40 +0530

    core-foundation-of-java

---
+++ Foundation-java/SimplePatten.java
@@ -0,0 +1,8 @@
+public class SimplePatten{
+    public static void main(String[] args){
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+    }
+}
---
+++ Foundation-java/SumOfTwoNumbers.java
@@ -0,0 +1,8 @@
+public  class SumOfTwoNumbers{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 30;
+        int sum = a +b;
+        System.out.println("Sum of two numbers:" + sum);
+    }
+}
