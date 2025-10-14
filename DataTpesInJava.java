12f98c65a3934b27e2659882be424b8ecbc38de5
rohitsolanki01 <rohitsolanki0473@gmail.com>
2025-10-14 19:03:48 +0530

    data types

---
+++ DataTpesInJava.java
@@ -0,0 +1,17 @@
+public class DataTpesInJava{
+    public static void main(String[] args){
+        // Primitive Data Types
+
+        int myNum = 5; 
+        System.out.println(myNum);              // Integer (whole number)
+        float myFloatNum = 5.99f;    // Floating point number
+        char myLetter = 'D';         // Character
+        boolean myBool = true;       // Boolean
+        String myText = "Hello";     // String (not a primitive type, but commonly
+        
+        long myLong = 1200283838484
+        double myDouble = 1234.56787
+        short myShort = 5000
+
+    }
+}
